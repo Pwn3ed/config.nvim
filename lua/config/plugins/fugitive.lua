c36a483d943c6a3deab31d184d8f0e8c4dbda26f
leonardo-luz <leonardoluz.f02@gmail.com>
2025-03-24 16:35:10 -0300

    fix: code type

--- lua/config/plugins/fugitive.lua
+++ lua/config/plugins/fugitive.lua
@@ -4,6 +4,6 @@ return {
         local set = vim.keymap.set
 
         set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
-        set("n", "<leader>gc", vim.cmd.Gdiffsplit { desc = "[G]it [C]hanges" })
+        set("n", "<leader>gc", vim.cmd.Gdiffsplit, { desc = "[G]it [C]hanges" })
     end
 }
